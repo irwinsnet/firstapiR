77eca1a0e226512c77ab735229580bb2371fe1a0
Stacy Irwin <stacy.k.irwin@gmail.com>
2016-12-12 10:26:24 -0600

    Modified GetAll test to use local data.

--- tests/testthat/test_90_util.R
+++ tests/testthat/test_90_util.R
@@ -11,10 +11,8 @@ if(exists("username") & exists("key")) {
   sess_http_valid <- TRUE
 }
 
-test_that("GetAll via HTTP", {
-  if(!sess_http_valid) skip("No username or authorization key")
-
-  archimedes <- GetAll(sess_http, "ARCHIMEDES")
+test_that("GetAll via local data", {
+  archimedes <- GetAll(sess_local, "ARCHIMEDES")
   expect_length(archimedes, 16)
   expect_named(archimedes, c("season", "event", "teams", "schedule.qual",
                        "schedule.playoff", "hybrid.qual", "hybrid.playoff",
@@ -25,5 +23,5 @@ test_that("GetAll via HTTP", {
   expect_is(archimedes$results.qual, "Results")
   expect_is(archimedes$results.playoff, "Results")
   expect_length(archimedes$results.qual, 49)
-  expect_equal(nrow(archimedes$results.qual), 750)
+  expect_equal(nrow(archimedes$results.qual), 468)
 })
